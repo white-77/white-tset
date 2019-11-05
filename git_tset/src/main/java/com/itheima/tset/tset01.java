ef8fd684c0a56b536c167b73e7eba3374b6240be
white <961024192@qq.com>
2019-11-05 20:47:09 +0800

    2019-11-5-20:48

---
+++ git_tset/src/main/java/com/itheima/tset/tset01.java
@@ -0,0 +1,14 @@
+package com.itheima.tset;
+
+/**
+ * @ProjectName: git_tset
+ * @Package: com.itheima.tset
+ * @ClassName: tset01
+ * @Author: HP
+ * @Description:
+ * @Date: 2019/11/5 20:39
+ * @Version: 1.0
+ */
+public class tset01 {
+    String aa;
+}
